b0108e3b25d0c8cb6d4cdb33b6cdb61fc8a567aa
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-20 21:32:29 +0530

    create three canvas for each event, Live, recent and upcoming

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import tkinter
 from dotenv import load_dotenv
 from fetch_data import Fetch_Data
 
-
 class UserInterface:
 
     load_dotenv()
@@ -12,11 +11,30 @@ class UserInterface:
     def __init__(self):
 
         self.cric_obj = Fetch_Data(os.getenv('rapid_api_key'), os.getenv('radip_api_host'), os.getenv('end_point_cricketbuzz'))
+        self.cric_data = self.cric_obj.get_matches(event_type= "live")
 
         self.window = tkinter.Tk()
         self.window.title("Cricket Buzz")
-        self.window.geometry("1000x800")
+        self.window.geometry("1200x800")
         self.window.config(bg="lavender")
+
+        # add title.
+        self.title = tkinter.Label(self.window, text= self.cric_data['response']['appIndex']['seoTitle'], font= ('arial', 30, 'bold'),
+                                   bg="lavender", foreground= "black")
+        self.title.place(x=250, y=50)
+
+        # Live events.
+        self.live_canvas = tkinter.Canvas(self.window, bg= "gainsboro", width= 300, height= 600, highlightthickness= 1)
+        self.live_canvas.place(x=100, y=150)
+
+        # Recent events.
+        self.recent_canvas = tkinter.Canvas(self.window, bg= "gainsboro", width= 300, height= 600, highlightthickness= 1)
+        self.recent_canvas.place(x= 450, y=150)
+
+        # Upcoming event.
+        self.upcoming_canvas = tkinter.Canvas(self.window, bg= "gainsboro", width= 300, height= 600, highlightthickness= 1)
+        self.upcoming_canvas.place(x= 800, y=150)
+
 
     def exit(self):
         self.window.mainloop()
